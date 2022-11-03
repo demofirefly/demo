178901bf58e2bdbf4d92bc7962a6f4f0d9c07420
demofirefly <116737692+demofirefly@users.noreply.github.com>
2022-11-03 14:05:39 +0200

    Update infl-6363ae5c349700ee3408819f.tf

--- infl-6363ae5c349700ee3408819f.tf
+++ infl-6363ae5c349700ee3408819f.tf
@@ -1,11 +1,10 @@
 resource "aws_subnet" "a4-demo-public-eu-west-1c-9bd" {
-  cidr_block                          = "20.10.13.0/24"
+  cidr_block                          = "20.23.13.0/24"
   map_public_ip_on_launch             = true
   private_dns_hostname_type_on_launch = "ip-name"
   tags = {
     Environment = "demo"
     Name        = "a4-demo-public-eu-west-1c"
-    Terraform   = "true"
   }
   vpc_id = "${data.aws_vpc.a4-demo-94b.id}"
 }
