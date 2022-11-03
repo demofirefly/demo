f33164a2ceb9b9a8886e3f6f4b8dc835479420ec
demofirefly <116737692+demofirefly@users.noreply.github.com>
2022-11-03 14:04:45 +0200

    Codification: aws_subnet

---
+++ infl-6363ae5c349700ee3408819f.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "a4-demo-public-eu-west-1c-9bd" {
+  cidr_block                          = "20.10.13.0/24"
+  map_public_ip_on_launch             = true
+  private_dns_hostname_type_on_launch = "ip-name"
+  tags = {
+    Environment = "demo"
+    Name        = "a4-demo-public-eu-west-1c"
+    Terraform   = "true"
+  }
+  vpc_id = "${data.aws_vpc.a4-demo-94b.id}"
+}
+
+
+
+
+data "aws_vpc" "a4-demo-94b" {
+  id = "vpc-0507d25e6a7164abd"
+}
+
