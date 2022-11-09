9ec87f012422f78cc23b5fbcc95c0ce899c8af18
lionfirefly <103496218+lionfirefly@users.noreply.github.com>
2022-11-09 18:21:22 +0200

    Codification: aws_s3_bucket

---
+++ infl-636bd381af665b946e4b2c83.tf
@@ -0,0 +1,6 @@
+resource "aws_s3_bucket" "logs-patient-mayfly-9-demo-79a" {
+  arn            = "arn:aws:s3:::logs-patient-mayfly-9-demo"
+  bucket         = "logs-patient-mayfly-9-demo"
+  hosted_zone_id = "Z1BKCTXD74EZPE"
+}
+
